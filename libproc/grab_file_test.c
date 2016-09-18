8497ae517ccba767f3127bf859277699b4804212
Gordon Marler <gmarler@gmarler.com>
2016-09-18 17:52:48 -0400

    Properly handle Pgrab_file symbol resolution, first pass

--- libproc/grab_file_test.c
+++ libproc/grab_file_test.c
@@ -6,8 +6,16 @@
 #include <sys/procfs.h>
 #include "libproc.h"
 
+/* used to pass information between Pobject_iter()'s caller and callback */
+typedef struct {
+  struct ps_prochandle  *file_pshandle;
+  long                   function_count;
+  /* TODO: Add something to show what the type of the file is, so we know how to
+   * handle symbol resolution properly - static a.out or dynamic library */
+} data_t;
+
 int proc_object_iter(void *, const prmap_t *, const char *);
-int file_object_iter(void *, const prmap_t *, const char *);
+int function_iter(void *arg, const GElf_Sym *sym, const char *func_name);
 
 int main(int argc, char **argv)
 {
@@ -31,31 +39,57 @@ int main(int argc, char **argv)
     exit(2);
   }
 
-  /* NOTE: Passing pshandle in as cd argument for use by Psymbol_iter later */
-  Pobject_iter(pshandle, proc_object_iter, (void *)pshandle);
+  /* NOTE: Passing pshandle in as cd argument for use as first argument of
+   * Psymbol_iter later */
+  Pobject_iter(pshandle, proc_object_iter, (void *)NULL);
 
   Pfree(pshandle);
 }
 
 int
-proc_object_iter(void *Proc, const prmap_t *pmp, const char *object_name)
+proc_object_iter(void *callback_arg, const prmap_t *pmp, const char *object_name)
 {
+  data_t                procfile_data;
   struct ps_prochandle *file_pshandle;
   int                   perr;
 
-  printf("%-120s\n", object_name);
+
+  printf("proc_object_iter: %-120s\n", object_name);
   /* For each object name, grab the file, then iterate over the objects,
-   * extracting the symbol table */
+   * extracting their symbol tables */
   if ((file_pshandle = Pgrab_file(object_name, &perr)) == NULL) {
     printf("Unable to grab file: %s\n",Pgrab_error(perr));
   }
+  /* NOTE: Passing file_pshandle in for use as callback argument for
+   * Psymbol_iter later */
+  procfile_data.file_pshandle = file_pshandle;
+  procfile_data.function_count = 0;
+
+  Psymbol_iter(file_pshandle,
+               object_name,
+               PR_SYMTAB,
+               BIND_GLOBAL | TYPE_FUNC,
+               function_iter,
+               (void *)&procfile_data);
+
+  printf("FUNCTION COUNT: %ld\n", procfile_data.function_count);
+
   return 0;
 }
 
+
+
 int
-file_object_iter(void *Proc, const prmap_t *pmp, const char *object_name)
+function_iter(void *callback_arg, const GElf_Sym *sym, const char *sym_name)
 {
-  printf("%-120s\n", object_name);
+  data_t procfile_data = (*((data_t *)callback_arg));
+
+  if (sym_name != NULL) {
+    printf("\t%32s %llu %llu\n", sym_name, sym->st_value, sym->st_size);
+    (((data_t *)callback_arg)->function_count)++;
+  } else {
+    printf("\tNULL FUNCNAME\n");
+  }
   return 0;
 }
 
