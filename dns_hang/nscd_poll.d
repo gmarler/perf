d8f10344d9bb24847493002e1867ab3662b00632
Gordon Marler <gmarler@gmarler.com>
2016-10-17 22:33:03 -0400

    print fd fs type

--- dns_hang/nscd_poll.d
+++ dns_hang/nscd_poll.d
@@ -16,9 +16,25 @@ syscall::pollsys:entry
   printf("%s:%s fd = %d, events = %x, revents = %x\n",
           probefunc, probename,
           this->pfds->fd, this->pfds->events, this->pfds->revents);
+
+  printf("FS TYPE: %s\n",fds[this->pfds->fd].fi_fs);
 }
 
 /* Only if fd is sockfs
  * Only if fd is UDP socket
  * Only if fd remote host is one of the BDNS servers
  */
+
+syscall::pollsys:return
+/self->in_pollsys/
+{
+  printf ("RETURN pid: %d Pollsys thread = %d returns %d:%d\n",
+          pid, tid, arg0, arg1);
+  this->pfds= (struct pollfd*) copyin(self->pfds_user, sizeof(struct pollfd));
+
+  printf("%s:%s fd = %d, events = %x, revents = %x\n",
+         probefunc, probename,
+         this->pfds->fd, this->pfds->events, this->pfds->revents);
+
+  self->in_pollsys = 0;
+}
