1bb196d6ae3cad00863f8980f462bdeaf5b4951f
Gordon Marler <gmarler@bloomberg.net>
2017-04-21 17:55:08 -0400

    Add aio_listio_test

--- async_to_sync_writes/aio_listio_test.cc
+++ async_to_sync_writes/aio_listio_test.cc
@@ -24,8 +24,8 @@ using std::vector;
 
 int main()
 {
-  int                           i;
-  aiocb_t          *control_block;
+  int                            i;
+  aiocb_t           *control_block;
   vector<aiocb_t *>  control_blocks;
 
   std::cout << "BEGIN" << std::endl;
@@ -62,40 +62,42 @@ int main()
   std::cout << "Writing random data to file" << std::endl;
   srand(time(NULL));
 
-  while (iterations--) {
-    /* buffer_number = random_at_most(BUFFERS - 1); */
-    buffer_number = ( rand() % BUFFERS );
+  for (int i = 0; i < iterations; i += 18) {
+    // lio_listio only supports a certain number of aiocb's - 18 in this case
+    for (int j = 0; j < 18; j++) {
 
-    control_block = (aiocb_t *)malloc(sizeof(aiocb_t));
+      /* buffer_number                         = random_at_most(BUFFERS - 1); */
+      buffer_number                            = ( rand() % BUFFERS );
 
-    control_block->aio_fildes  = fd;
-    control_block->aio_offset  = 0;
-    control_block->aio_sigevent.sigev_notify = SIGEV_NONE;
-    control_block->aio_nbytes  = BUFSIZE;
-    control_block->aio_buf     = buffers[buffer_number];
-    control_block->aio_reqprio = 0;
+      control_block                            = (aiocb_t *)malloc(sizeof(aiocb_t));
 
-    control_blocks.push_back(control_block);
+      control_block->aio_fildes                = fd;
+      control_block->aio_offset                = 0;
+      control_block->aio_sigevent.sigev_notify = SIGEV_NONE;
+      control_block->aio_nbytes                = BUFSIZE;
+      control_block->aio_buf                   = buffers[buffer_number];
+      control_block->aio_reqprio               = 0;
+      /* Using lio_listio() here */
+      control_block->aio_lio_opcode            = LIO_WRITE;
 
-    int ret = aio_write(control_block);
-    if (ret == -1) {
-      perror("aio_write");
+      control_blocks.push_back(control_block);
     }
-
+    int ret = lio_listio(LIO_WAIT,&control_blocks[0],(int)control_blocks.size(),NULL);
+    if (ret == 0) {
+      // std::cout << "lio_listio SUCCESSFUL" << std::endl;
+    } else {
+      perror("lio_listio FAILED");
+      return(1);
+    }
+    // clear control_blocks vector
+    control_blocks.clear();
   }
   // Iterate over vector of control blocks, checking that each is complete
   // before closing
   // Note that this isn't strictle necessary, and slows us down a bit
   std::cout << "CONFIRMING I/O made it to disk" << std::endl;
-  for (auto it = control_blocks.begin();
-       it != control_blocks.end(); it++) {
-    int ret = aio_suspend(&*it, 1, NULL);
-    if (ret != 0) {
-      perror("aio_suspend");
-    }
-  }
   close(fd);
 
 
-  return 0;
+  return(0);
 }
