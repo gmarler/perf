7e21215312260887aeca007955b61707a904874d
Gordon Marler <gmarler@bloomberg.net>
2017-08-18 11:33:53 -0400

    debug message after args parsed

--- async_to_sync_writes/main.c
+++ async_to_sync_writes/main.c
@@ -13,4 +13,14 @@ int main(int argc, char **argv)
 
   collect_options(&argc, argv, filepath, &filesize, &blocksize, &test, &sync_type);
 
+  printf("Running with following options:\n");
+  printf("File to write: %s, File size %lld, I/O Block Size: %lld\n",
+         filepath, filesize, blocksize);
+  printf("I/O Test: %s, Synchronized Type: %s\n",
+         test == Test_pwrite ? "pwrite" :
+         test == Test_aio_write ? "aio_write" :
+         test == Test_lio_listio ? "lio_listio" : "UNKNOWN",
+         sync_type & O_SYNC ? "O_SYNC" :
+         sync_type & O_DSYNC ? "O_DSYNC" :
+         "No Synchronization");
 }
