cbea6f9c80d08edc57e1fc4c16094f55e9376517
Gordon Marler <gmarler@gmarler.com>
2017-08-30 10:27:56 -0400

    use pthread flag properly, aio_write test mostly complete

---
+++ async_to_sync_writes/aio_write_test.c
@@ -0,0 +1,159 @@
+#include "aio_write_test.h"
+
+typedef struct {
+  pthread_mutex_t    mutex;
+  pthread_cond_t     condvar;
+  struct aiocb      *control_blocks;
+  /* the semantics here can be odd:
+   * - If O_SYNC/O_DSYNC set on file descriptor, this means the I/O made it to
+   *   stable storage
+   * - If the above not set, this just means the I/O has been handed off to the
+   *   kernel, no more
+   */
+  long               ios_completed;
+  long               total_aios_to_complete;
+  sigset_t           set;
+} global_data_t;
+
+typedef struct {
+  global_data_t  *global_info;
+  struct aiocb   *cblock;
+} per_io_data_t;
+
+void aio_write_test(int fd, long long filesize, long long blocksize,
+                    char *buffers, int buffer_count)
+{
+  unsigned long      iterations = filesize / blocksize;
+  long               buffer_number;
+  char              *buffer;
+  struct aiocb      *control_block;
+  sigset_t           set;
+  struct sigaction   act;
+  pthread_t          tid;
+  long               max_aios;
+  global_data_t      central_global_data;
+
+  /* Initialize mutex and condition variable
+   * WARNING: this is a one time initialization - attempting twice will cause an
+   *          error */
+  pthread_mutex_init(&central_global_data.mutex,NULL);
+  pthread_cond_init(&central_global_data.condvar,NULL);
+  central_global_data.ios_completed = 0; /* initialize to 0 */
+
+  max_aios = sysconf(_SC_AIO_MAX);
+  if (max_aios == -1) {
+    /* it's undefined, so select a sane default */
+    max_aios = 512;
+  }
+  printf("Max outstanding AIO I/Os: [%ld]\n",max_aios);
+
+  /* don't know how long to make the array until you determine max_aios */
+  struct aiocb      *control_blocks[max_aios];
+
+  /* Disable reception of MYSIG_AIO_COMPLETE/MYSIG_STOP in the main thread */
+  sigemptyset(&set);
+  sigaddset(&set,MYSIG_AIO_COMPLETE);
+  sigaddset(&set,MYSIG_STOP);
+  pthread_sigmask(SIG_BLOCK, &set, NULL);
+  central_global_data.set                    = set;
+  central_global_data.total_aios_to_complete = iterations;
+
+  /* Clear out the sigaction */
+  memset(&act,0,sizeof(act));
+  act.sa_flags = SA_SIGINFO;
+  /* act.sa_sigaction = io_completion_handler; */
+  /* Seperate thread will handle signals instead */
+  act.sa_sigaction = NULL;
+
+  /* Start the I/O completion handling thread */
+  pthread_create(&tid,NULL,sig_thread,&central_global_data);
+
+  /*
+  per_io_list_sigevent.sigev_notify          = SIGEV_SIGNAL;
+  per_io_list_sigevent.sigev_signo           = MYSIG_AIO_COMPLETE;
+  per_io_list_sigevent.sigev_value.sival_ptr = (void *)&control_blocks;
+  */
+
+  /* Enable reception of SIGRTMAX/SIGRTMAX-1 in the I/O completion handling
+   * thread */
+  srand(time(NULL));
+
+  for (int i = 0; i < iterations; i += max_aios) {
+    for (int j = 0; j < max_aios; j++) {
+
+      /* buffer_number                         = random_at_most(BUFFERS - 1); */
+      buffer_number                            = ( rand() % buffer_count );
+
+      control_block                            = (struct aiocb *)malloc(sizeof(struct aiocb));
+
+      control_block->aio_fildes                = fd;
+      control_block->aio_offset                = 0;
+      /* Don't bother signal notification per aiocb, just get one signal when
+       * they're all done */
+      control_block->aio_sigevent.sigev_notify = SIGEV_SIGNAL;
+      control_block->aio_sigevent.sigev_signo  = MYSIG_AIO_COMPLETE;
+      control_block->aio_nbytes                = blocksize;
+      control_block->aio_buf                   = buffers + (buffer_number * blocksize);
+      control_block->aio_reqprio               = 0;
+
+      control_blocks[j]                        = control_block;
+
+      /* Check individual I/O submission (not completion) status here */
+      int ret = aio_write(control_block);
+      if (ret == 0) {
+      } else {
+        perror("aio_write() initiation FAILED");
+        /* return(1); */
+      }
+
+    }
+
+
+    /* Wait for I/O completion handling thread to assert the condition
+     * variable to continue on */
+    pthread_cond_wait(&central_global_data.condvar,&central_global_data.mutex);
+
+    /* clear control_blocks vector */
+    /* control_blocks.clear(); */
+    for (int i = 0;  i < max_aios; i++) {
+      /* TODO: we can reuse the aiocbs, so just clear them here  */
+    }
+  }
+
+  /* Join with the I/O handling thread when we're done */
+  pthread_join(tid,NULL);
+}
+
+static void io_completion_handler()
+{
+
+}
+
+static void *sig_thread(void *arg)
+{
+  int            signum;
+  siginfo_t      info;
+  global_data_t *global_data = (global_data_t *)arg;
+  struct aiocb  *my_aiocb;
+
+  printf("Entered sig_thread\n");
+  do {
+    signum = sigwaitinfo(&(global_data->set), &info);
+    if (signum == MYSIG_AIO_COMPLETE) {
+      /* cast needed: (global_data_t *)info.si_value.sival_ptr */
+      my_aiocb = (struct aiocb *)info.si_value.sival_ptr;
+      global_data->ios_completed++;
+      printf("%ld aio_write completions handled\n",global_data->ios_completed);
+      if (global_data->ios_completed >= global_data->total_aios_to_complete) {
+        printf("All aio_write() activities completed\n");
+        break;
+      }
+    } else if (signum == MYSIG_STOP) {
+      /* Probably not needed for this scenario anyway */
+      return (void *)true;
+    }
+  } while (signum != -1 || errno == EINTR);
+
+  pthread_cond_signal(&(global_data->condvar));
+  return (void *)true;
+}
