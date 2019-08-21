bd3c5655b21e07e94e4df15f057825332e094e38
Gordon Marler <gmarler@bloomberg.net>
2019-08-21 13:11:50 -0400

    some tests to add

--- resource_controls/memory_alloc.c
+++ resource_controls/memory_alloc.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
 
 #define SZ         32
 
@@ -13,11 +14,17 @@ int main(int argc, char **argv)
   for (n = 0; n < SZ; n++) {
     pp[n] = malloc(1<<20);  /* 1 MB */
     if (pp[n] == NULL) {
+      perror("UNEXPECTED malloc failure");
+      printf("ERRNO: %d\n", errno);
       printf("malloc failure after %d MB\n", n);
-      /* exit(1); */
+      exit(1);
     }
   }
   pp[SZ] = malloc(1);
+  if (pp[SZ] == NULL) {
+    perror("EXPECTED malloc failure");
+    exit(0);
+  }
 
   for (i = 0; i < n; i++) {
     usleep(250000);
